59c306f8573ae69071ecc743b6238c306bf7e899
Ruan Arcega <ruanarcega@gmail.com>
2019-12-05 01:36:23 -0300

    fix: missing dot in state

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route { ...rest } render={props => (
         isAuthenticated() ? (
-            <Component {..props} />
+            <Component {...props} />
         ) : (
             <Redirect to={ {pathname: '/', state: { from: props.location} } } />
         )
