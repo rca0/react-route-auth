9e1550ed396d2cb0ace376d01fa4c41ab85dea28
Ruan Arcega <ruanarcega@gmail.com>
2019-12-05 01:35:54 -0300

    feat: implement private route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { isAuthenticated } from './auth'
 
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route { ...rest } render={props => (
+        isAuthenticated() ? (
+            <Component {..props} />
+        ) : (
+            <Redirect to={ {pathname: '/', state: { from: props.location} } } />
+        )
+    )
+  }
+/>
+)
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={} => <h1>Hello World!</h1>} />
+            <Route exact path='/' component={() => <h1>Hello World!</h1>} />
+            <PrivateRoute path='/app' component={() => <h1>You are logged</h1>} />
         </Switch>
     </BrowserRouter>
 )
